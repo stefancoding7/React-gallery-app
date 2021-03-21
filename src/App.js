8c930498d9d5d4b3da7606f0cedad4a483447cb5
Stefan Caky <stefan.caky777@gmail.com>
2021-03-21 20:50:14 +0000

    template
    Template ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
+import './style/css/index.css';
+import apiKey from './config';
+import Form from './components/Form';
+import Photo from './components/Photo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    state = {
+      data: [],
+      loading: true
+    }
+
+    searchApi(query) {
+        axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
+        .then(response => {
+            this.setState({ 
+              data: response.data.photos.photo,
+              loading: false
+            })
+        })
+        .catch(error => {
+          console.log('Error fetching and parsing data', error);
+        });
+
+       
+    }
+    
+    
+    render() {
+        return (
+         <div>
+              <Form />
+              <Photo />
+         </div>
+            
+        )
+    }
 }
 
 export default App;
